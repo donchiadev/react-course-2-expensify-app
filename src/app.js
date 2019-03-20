88381bd4d8eb1c106724800ea5067fc50e32a88a
Antonio D'Onchia <antonio.donchia@hotmail.com>
2019-03-20 17:01:40 +0100

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // React-Redux
 import { Provider } from  'react-redux'
-// Imported moment library only to create some dummy data
-import moment from 'moment';
 // Application routers
 import AppRouter from './routers/AppRouter'
 // Application store (actually the configuration function)
@@ -13,43 +11,15 @@ import configureStore from './store/configureStore'
 import { addExpense, removeExpense } from './actions/expenses'
 // Filters action generators
 import { setTextFilter } from './actions/filters'
-//
+// Expenses selector
 import getVisibileExpenses from './selectors/expenses'
-// Stles imports
+// Styles imports
 import 'normalize.css/normalize.css'
 import './styles/style.scss'
 // Style for the date picker
 import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
-
-// store.subscribe(() => {
-//     const { expenses, filters } = store.getState()
-//     const visibleExpenses = getVisibileExpenses(expenses, filters)
-//     console.log(visibleExpenses)
-// })
-
-store.dispatch(addExpense({
-    description: 'Water Bill',
-    amount: 3500,
-    note: 'Just the water bill',
-    createdAt: moment().valueOf()
-}))
-
-store.dispatch(addExpense({
-    description: 'Gas Bill',
-    amount: 5000,
-    note: 'Just the gas bill',
-    createdAt: moment().valueOf()
-}))
-
-
-store.dispatch(addExpense({
-    description: 'Rent Bill',
-    amount: 109500,
-    note: 'Just the rent bill',
-    createdAt: moment().valueOf()
-}))
 
 const jsx = (
     <Provider store={store}>
